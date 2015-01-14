16ef8cd2f9f53e54a7421516dd1061ab9d32a34f
Eric Peterson <i.am@eric.pe>
2015-01-14 11:12:21 -0800

    Adding support for User translation via Entity Translation module. #3

---
+++ src/Drupal/Translatable/Entity/UserTranslatable.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Defines a User translatable compatible with Entity Translation module.
+ */
+
+namespace EntityXliff\Drupal\Translatable\Entity;
+
+use EntityXliff\Drupal\Translatable\EntityFieldTranslatableBase;
+
+
+/**
+ * Class UserTranslatable
+ * @package EntityXliff\Drupal\Translatable\Entity
+ */
+class UserTranslatable extends EntityFieldTranslatableBase {
+
+}
