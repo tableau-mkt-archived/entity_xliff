84501e34ba4649171a2c81c3444a5921297080c0
Eric Peterson <i.am@eric.pe>
2017-08-30 16:02:30 -0700

    Resolve link field handler test issues.

--- test/Drupal/FieldHandlers/LinkFieldHandlerTest.php
+++ test/Drupal/FieldHandlers/LinkFieldHandlerTest.php
@@ -13,7 +13,7 @@ class LinkFieldHandlerTest extends \PHPUnit_Framework_TestCase {
    *
    * @test
    */
-  public function getValue() {
+  public function getValueTitle() {
     $expectedResponse = 'value';
 
     $observerWrapper = $this->getMock('\EntityMetadataWrapper', array('value'));
@@ -22,7 +22,28 @@ class LinkFieldHandlerTest extends \PHPUnit_Framework_TestCase {
       ->willReturn(array('title' => $expectedResponse));
 
     $handler = new LinkFieldHandler();
-    $this->assertEquals($expectedResponse, $handler->getValue($observerWrapper));
+    $value = $handler->getValue($observerWrapper);
+    $this->assertEquals($expectedResponse, $value['title']['#text']);
+    $this->assertEquals('Title text', $value['title']['#label']);
+  }
+
+  /**
+   * Tests that the link field handler returns the "url" key on the array
+   * returned by $wrapper->value();
+   * @test
+   */
+  public function getValueUrl() {
+    $expectedResponse = 'http://example.com/en-us';
+
+    $observerWrapper = $this->getMock('\EntityMetadataWrapper', array('value'));
+    $observerWrapper->expects($this->once())
+      ->method('value')
+      ->willReturn(array('url' => $expectedResponse));
+
+    $handler = new LinkFieldHandler();
+    $value = $handler->getValue($observerWrapper);
+    $this->assertEquals($expectedResponse, $value['url']['#text']);
+    $this->assertEquals('Link URL', $value['url']['#label']);
   }
 
 }
